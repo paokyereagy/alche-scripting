1f804d73d0ded89a718072ecb38f8b5908b28790
paokyereagy <p.okyereagy@alustudent.com>
2026-07-06 09:40:21 -0700

    pop

---
+++ regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+input = ARGV[0]
+
+if input.nil?
+  puts "Usage: ruby script.rb <string>"
+  exit
+end
+
+regex = /^hb?tn$/
+
+if input.match?(regex)
+  puts "#{input}"
+else
+  puts ""
+end
